96e4bd4a3a76de2b3c78d27e245c809072cbe00e
Ascari <carlos.ascari.x@gmail.com>
2016-05-25 03:57:28 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# github-people-finder
+
+[![Alt Description](http://i.imgur.com/fiYZVoJ.png)](http://i.imgur.com/fiYZVoJ.png)
+*Click on the image to see it full size*.
+
+* Users and Organizations are listed from those with repositories with the highest number of stars to the least
+* Any public contact information is displayed and all repos that belong to each entity will be added as they are scraped.
+* All links are `_blank`, they will open a new tab, so you can move around while the download progresses.
+
+### USAGE
+You need to edit `index.js`, so it includes your own client id and secret:
+
+```js
+var CLIENT_ID = '';
+var CLIENT_SECRET = '';
+```
+otherwise Github will [limit your requests down to 60, instead of 5000](https://developer.github.com/v3/#rate-limiting).
+
+Thats all, just open `index.html` on your browser and it will begin scraping.
+
+### FYI
+
+The search parameters are hardcoded at the bottom of `index.js`:
+
+`https://api.github.com/search/repositories?q=created:%3E2015-12-01&sort=stars&per_page=100`
+
+You may want to tweak it
+
+[MIT License]()
---
+++ index.js
@@ -0,0 +1,141 @@
+var UNIQUE_USERS = {}, TOTAL = 0, COUNT = 0, DISABLE_USER_LOOKUP = false, HALT = false;
+var CLIENT_ID = '';
+var CLIENT_SECRET = '';
+var MAX = CLIENT_ID && CLIENT_SECRET ? Infinity : 200;
+
+// -----------------------------------------------------------------------------
+
+function nolimit(x) {
+	if (!CLIENT_ID || !CLIENT_SECRET) return '';
+	return (x||'') + 'client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+}
+
+function contactElement(key, value, link) {
+	if (link) {
+		var hasProtocol = value.indexOf('http') === 0;
+		return $('<dl>')
+		.append(
+			$('<dt>').text(key),
+			$('<dd>').append(
+				$('<a>').attr({'href': hasProtocol ? value : 'http://' + value, target: '_blank'}).text(value)
+			)
+		);		
+	}
+
+	return $('<dl>')
+	.append(
+		$('<dt>').text(key),
+		$('<dd>').text(value)
+	);
+}
+
+function userElement(owner) {
+	var entity = $('.entity-container.template').clone().removeClass('template');
+	entity.find('.avatar img').attr('src', owner.avatar_url);
+	entity.find('.username a').text(owner.login).attr('href', owner.html_ur);
+	entity.find('.template').remove();
+	return entity;
+}
+
+function repoElement(repo) {
+	var repo_element = $('.template .persona.repo.template').clone().removeClass('template');
+	repo_element.find('.title a').text(repo.name).attr('href', repo.html_url);
+	repo_element.find('p').text(repo.description);
+	repo_element.find('.stat.stars b').text(repo.stargazers_count);
+	repo_element.find('.stat.watchers b').text(repo.watchers_count);
+	repo_element.find('.stat.forks b').text(repo.forks);
+	repo_element.find('.stat.size b').text(repo.size);
+	return repo_element;
+}
+
+function pump(url) {
+	if (!url || (TOTAL && COUNT >= TOTAL) || HALT) return;
+
+	if (COUNT >= MAX) return console.warn('MAX reached!');
+
+	var $res = $('#res');
+	
+	$
+	.get(url)
+	.done(function(result, status, req){
+		$('.total').text(TOTAL = result.total_count);
+		var headers = req.getAllResponseHeaders();
+
+		COUNT += result.items.length;
+		var percent = COUNT/TOTAL;
+		$('.count').text(COUNT);
+		$('.percent').text(percent.toFixed(6) + '%');
+		$('.progress .bar').css('width'. percent + '%');
+
+		var tmp_count = result.items.length;
+		var tmp_elements = [];
+		result.items.forEach(function(item){
+			var owner_username = item.owner.login;
+			var owner_type = item.owner.type;
+
+			if (!UNIQUE_USERS[owner_username]) {
+				var user_element = userElement(item.owner);
+				tmp_elements.push(user_element);
+				UNIQUE_USERS[owner_username] = {
+					owner: item.owner,
+					element: user_element,
+					repos: []
+				};
+			}
+			var repo_element = repoElement(item);
+			UNIQUE_USERS[owner_username].repos.push(repo_element);
+			UNIQUE_USERS[owner_username].element.find('.entity').append(repo_element);
+
+			if (UNIQUE_USERS[owner_username].user || DISABLE_USER_LOOKUP || HALT) {
+				tmp_count--;
+				if (tmp_count <= 0) next();
+				return;
+			}
+
+			$
+			.get(owner_type === 'User' ? 'https://api.github.com/users/' + owner_username + nolimit('?') : 'https://api.github.com/orgs/' + owner_username + nolimit('?'))
+			.done(function(result, status, req){
+				UNIQUE_USERS[owner_username].user = result;
+				var contactContainerElement = UNIQUE_USERS[owner_username].element.find('.contact');
+				UNIQUE_USERS[owner_username].element.find('.name').text(result.name);
+				if (result.company) contactContainerElement.append(contactElement('Company', result.company));
+				if (result.email) contactContainerElement.append(contactElement('Email', result.email));
+				if (result.blog) contactContainerElement.append(contactElement('Blog', result.blog, true));
+				if (result.location) contactContainerElement.append(contactElement('Location', result.location));
+				if (result.bio) contactContainerElement.append(contactElement('Bio', result.bio));
+				if (result.followers) contactContainerElement.append(contactElement('Followers', result.followers));
+				tmp_count--;
+
+				if (tmp_count <= 0) next();
+			})
+			.fail(function (res) {
+				next(res.responseJSON);
+			});
+		});
+
+		function next(error) {
+			if (tmp_elements.length) $res.append.apply($res, tmp_elements);
+
+			if (error) {
+				DISABLE_USER_LOOKUP = true;
+				$('#error').show().text(error.message + '\nContact information fetching has been disabled.');
+			}
+
+			var match = /<(.*)>/.exec(headers || '');
+			if (match && match[1]) {
+				if (HALT) return;
+				pump(match[1] + nolimit('&'));
+			}
+		}
+	})
+	.fail(function(res) {
+		HALT = true;
+		$('#error').show().text(res.responseJSON.message + '\nFetching has stopped.');
+	});
+}
+
+// -----------------------------------------------------------------------------
+
+$(function start() {
+	pump('https://api.github.com/search/repositories?q=created:%3E2015-12-01&sort=stars&per_page=100' + nolimit('&'));
+});
